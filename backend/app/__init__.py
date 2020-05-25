c8c3bab303430c924de5e07ea00cd4e29a1b952d
Sunnywillow <Joseph3588.me@gmail.com>
2020-05-25 14:36:55 +0800

    2.Vue.js通过axios访问Flask RESTful API

---
+++ backend/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+from config import Config
+
+def create_app(config_class=Config):
+    app = Flask(__name__)  # 实例化Flask
+    app.config.from_object(config_class)
+
+    # enable CORS
+    CORS(app)
+
+    # 注册 blueprint
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
---
+++ backend/app/api/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+bp = Blueprint('api', __name__)
+
+# 防止循环导入, ping.py也会导入bp
+from app.api import ping
