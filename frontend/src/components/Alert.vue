041461ad90cefc9933c5b7111f6c1b6076b53e7e
Sunnywillow <Joseph3588.me@gmail.com>
2020-05-26 23:27:26 +0800

    4. Vue.js调用API实现用户注册/登陆/退出

---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert" role="alert" v-bind:class="'alert-' + variant">
+      {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['variant', 'message']
+    }
+</script>
+
+<style scoped>
+
+</style>
